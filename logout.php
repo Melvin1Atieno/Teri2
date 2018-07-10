5df8225ffa8d4b43408f5b665100d14dd2759b70
naibor <liznaibor.en@gmail.com>
2018-07-10 15:59:17 +0300

    [Chore] Cleanup logout file

--- logout.php
+++ logout.php
@@ -5,16 +5,4 @@ session_start();
 unset($_SESSION["username"]);
 session_destroy();
 header('location: index.php');
-
-
-
-    // Removing session data
-    // if(isset($_SESSION["username"])){
-    //     $sql = "DELETE * FROM logged WHERE username=$username";
-    //     if($con->query($sql) === true){
-    //         echo "Records were deleted successfully.";
-    //     } else{
-    //         echo "ERROR: Could not able to execute $sql. " . $con->error;
-    //     }
-    // }
 ?>
