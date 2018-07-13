610702bf37b35034cbe81a56ddefe35723ba7e35
naibor <liznaibor.en@gmail.com>
2018-07-13 15:22:22 +0300

    [Fix]Fix merge add feature to add merge status and match found status

--- process.php
+++ process.php
@@ -15,21 +15,22 @@
         $result = mysqli_query($con,"INSERT INTO investments(PackageType, Amount,ExpectedReturns,InvestorsUsername,Paid,InvestmentDate)
             VALUES('$goldone','$amount','$expected_amount','$username', 'not_paid', NOW())");
     }
-    
-    $query = mysqli_query($con,"SELECT * FROM investments WHERE NoOfMatchesFound < 2 LIMIT 1");
+    //select user to merge
+    $query = mysqli_query($con,"SELECT * FROM investments WHERE Merged < 2 LIMIT 1");
     $Username = "";
     // echo "<Script>('$Usernam')</Script>";
     while ($row = $query->fetch_assoc()){
         // $Merged = (int)$row['Merged'];
         $id = (int)$row['InvestmentID'];
         $Username = $row['InvestorsUsername'];
-        $nquery = mysqli_query($con, "SELECT * FROM investments WHERE InvestorsUsername != '$Username' AND Merged < 2 ");
+        //select the user to merge I.e to be a match
+        $nquery = mysqli_query($con, "SELECT * FROM investments WHERE InvestorsUsername != '$Username' AND NoOfMatchesFound < 1 ");
         while ($nrow = $nquery->fetch_assoc()){
             $musername = $nrow['InvestorsUsername'];
             //update user who's been merged
-            $update = mysqli_query($con,"UPDATE investments SET Merged = Merged + 1 WHERE InvestorsUsername='$Username' AND InvestmentID='$id'");
+            $update = mysqli_query($con,"UPDATE investments SET Merged = 1+Merged  WHERE InvestorsUsername='$Username' AND InvestmentID='$id'");
            //update details for user who is a match
-            $mupdate = mysqli_query($con, "UPDATE investments SET NoOfMatchesFound = 1 WHERE InvestorsUsername='$musername'");
+            $mupdate = mysqli_query($con, "UPDATE investments SET NoOfMatchesFound = NoOfMatchesFound + 1 WHERE InvestorsUsername='$musername'");
             //insert details to matches table
             $tupdate = mysqli_query($con, "INSERT INTO matches (InvestmentID, MatchOneUsername) VALUES('$id', '$musername',)");
         }
